46ddae41b8c29335e8160c54aab93a775bbea0e0
jake hanes <jhanesx@yahoo.com>
2019-08-10 08:16:55 -0700

    created files and installed packages

---
+++ wamazon.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "wamazon_db"
+  });

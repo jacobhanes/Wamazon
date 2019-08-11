1aa74476c8fc3605eae386584325d2e5d3b22d69
jake hanes <jhanesx@yahoo.com>
2019-08-10 20:53:37 -0700

    updating works for crud

--- wamazon.js
+++ wamazon.js
@@ -2,15 +2,64 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "root",
-    database: "wamazon_db"
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "wamazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
+  askQuestions();
+});
+
+function askQuestions() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "start",
+      message: "What would you like to buy? type the products id",
+    },
+    {
+      type: "input",
+      name: "amount",
+      message: "how many?"
+    }
+  ])
+    .then(function (answer) {
+      let amount = parseInt(answer.amount);
+      let ident = parseInt(answer.start);
+      const query = connection.query("UPDATE products SET quantity = quantity - " + amount + " where ?", [
+
+        {
+          id: ident
+        }
+      ],
+        function (err, res) {
+          if (err) throw err;
+          console.log(res + " products updated!\n");
+
+        })
+      readProducts();
+      askQuestions();
+    })
+}
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+
+    console.log(res);
+
   });
+}
